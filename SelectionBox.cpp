4e81d04aa3cfdab8e7c3ce70b8a909330ebff46e
BalmunAzuresky <40233917+AnthonyWinrow@users.noreply.github.com>
2023-08-18 11:46:52 -0700

    Update SelectionBox.cpp

--- SelectionBox.cpp
+++ SelectionBox.cpp
@@ -1,7 +1,9 @@
 #include "E:/Unreal Projects/GarudaRift/Source/GarudaRift/ActorClasses/SelectionBox.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Engine/Classes/Components/StaticMeshComponent.h"
+#include "DrawDebugHelpers.h"
 #include "Engine/StaticMesh.h"
+#include "Materials/MaterialInterface.h"
 
 // Sets default values
 ASelectionBox::ASelectionBox()
@@ -21,7 +23,7 @@ ASelectionBox::ASelectionBox()
 	UE_LOG(LogTemp, Warning, TEXT("StaticMesh Component Created_selectionbox construct"));
 
 	// Load static mesh
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/GarudaRift/Meshes/Architecture/VFX/SelectionVolume.SelectionVolume")); 
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("StaticMesh'/Engine/BasicShapes/Cube.Cube'"));
 	if (MeshAsset.Succeeded())
 	{
 		StaticMeshComponent->SetStaticMesh(MeshAsset.Object);
@@ -34,6 +36,37 @@ ASelectionBox::ASelectionBox()
 		// Log: Mesh not found
 		UE_LOG(LogTemp, Warning, TEXT("StaticMesh Not Found_selectionbox construct"));
 	}
+
+	// Load material
+	static ConstructorHelpers::FObjectFinder<UMaterialInterface> MaterialAsset(TEXT("MaterialInstanceConstant'/Game/GarudaRift/Materials/VFx/BuildSelection.BuildSelection'"));
+	if (MaterialAsset.Succeeded())
+	{
+		StaticMeshComponent->SetMaterial(0, MaterialAsset.Object);
+
+		// Log: Material loaded succesfully
+		UE_LOG(LogTemp, Warning, TEXT("Material Loaded Successfully_selectionbox construct"));
+	}
+	else
+	{
+		// Log: Material Not Found
+		UE_LOG(LogTemp, Warning, TEXT("Material Not Found_selectionbox construct"));
+	}
+	// Set default location
+	FVector Location(1.f, 1.f, 1.f);
+	StaticMeshComponent->SetRelativeLocation(Location);
+
+	// Log the set location
+	UE_LOG(LogTemp, Warning, TEXT("StaticMeshLocation Set: %s_selectionbox construct"), *Location.ToString());
+
+	// Set the world scale of the static mesh component
+	FVector Scale(1.f, 1.f, 1.f);
+	StaticMeshComponent->SetWorldScale3D(Scale);
+
+	// Set the visibility of the static mesh component
+	StaticMeshComponent->SetVisibility(true);
+
+	// Log the visibility set
+	UE_LOG(LogTemp, Warning, TEXT("StaticMesh Visibility Set To True_selectionbox construct"));
 }
 
 // Called when the game starts or when spawned
@@ -49,4 +82,3 @@ void ASelectionBox::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
