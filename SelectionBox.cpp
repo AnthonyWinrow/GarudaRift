54ca150200b8be1529d528d940e81dd5ed214336
BalmunAzuresky <40233917+AnthonyWinrow@users.noreply.github.com>
2023-08-20 13:45:38 -0700

    Update SelectionBox.cpp

--- SelectionBox.cpp
+++ SelectionBox.cpp
@@ -5,8 +5,40 @@
 // Sets default values
 ASelectionBox::ASelectionBox()
 {
+	UE_LOG(LogTemp, Log, TEXT("SelectionBox Constructor Called_selectionbox_constructor"));
+
 	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Create spline component
+	WallSpline = CreateDefaultSubobject<USplineComponent>(TEXT("WallSpline"));
+
+	// Attach the spline to its root
+	WallSpline->SetupAttachment(RootComponent);
+
+	// Add initial control points
+	WallSpline->AddSplinePoint(FVector(0, 0, 0), ESplineCoordinateSpace::Local);
+	WallSpline->AddSplinePoint(FVector(100, 0, 0), ESplineCoordinateSpace::Local);
+
+	// Log the spline component initialization
+	UE_LOG(LogTemp, Log, TEXT("WallSpline Component Initialized_selectionbox_constructor"));
+	UE_LOG(LogTemp, Log, TEXT("Added Initial Control Points to WallSpline_selectionbox_constructor"));
+
+	// Find the mesh
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMeshAsset(TEXT("StaticMesh'/Engine/BasicShapes/Sphere.Sphere'"));
+	UStaticMesh* SphereMesh = SphereMeshAsset.Object;
+
+	// Create the control point visualizations
+	for (int i = 0; i < WallSpline->GetNumberOfSplinePoints(); ++i)
+	{
+		UStaticMeshComponent* ControlPointMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName(*FString::Printf(TEXT("ControlPointMesh%d"), i)));
+		ControlPointMesh->SetupAttachment(RootComponent);
+		ControlPointMesh->SetStaticMesh(SphereMesh);
+		ControlPointMsesh->SetWorldlocation(WallSpline->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::World));
+		ControlPointMeshes.Add(ControlPointMesh);
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("Control Points Initialized and Visible with Default Sphere Mesh_selectionbox_constructor"));
 
 	// Initialize the mesh component
 	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
@@ -39,17 +71,17 @@ void ASelectionBox::Tick(float DeltaTime)
 
 }
 
-void ASelectionBox::LeftClick(const FVector& ClickLocation)
-{
-	// Spawn the mesh at the given location
-	FActorSpawnParameters SpawnParams;
-	GetWorld()->SpawnActor<ASelectionBox>(ASelectionBox::StaticClass(), ClickLocation, FRotator(0), SpawnParams);
-
-	UE_LOG(LogTemp, Log, TEXT("Mesh Spawned on LeftClick at Location: %s_selectionbox_leftclick"));
-}
-
 void ASelectionBox::DestroyMesh()
 {
+	// Check if static mesh component is null
+	if (!StaticMeshComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("StaticMeshComponent is NULL in DestroyMesh_selectionbox_destroymesh"));
+		return;
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("DestroyMesh Called_selectionbox_destroymesh"));
+
 	// Check if static mesh component is still valid
 	if (this && !IsPendingKill())
 	{
