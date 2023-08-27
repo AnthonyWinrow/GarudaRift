2d75ea4f1ccdd59275187d465c732d0d71b4a12b
BalmunAzuresky <40233917+AnthonyWinrow@users.noreply.github.com>
2023-08-27 16:02:36 -0700

    Update SelectionBox.cpp

--- SelectionBox.cpp
+++ SelectionBox.cpp
@@ -1,11 +1,18 @@
 #include "E:/Unreal Projects/GarudaRift/Source/GarudaRift/ActorClasses/SelectionBox.h"
 #include "Materials/MaterialInstanceConstant.h"
 #include "UObject/ConstructorHelpers.h"
+#include "E:/Unreal Projects/GarudaRift/Source/GarudaRift/CharacterLogic/BuildMode.h"
 
 // Sets default values
 ASelectionBox::ASelectionBox()
 {
 	UE_LOG(LogTemp, Log, TEXT("SelectionBox Constructor Called_selectionbox_constructor"));
+
+	CurrentSelectedTag = TEXT("");
+	SelectedControlPoint = nullptr;
+
+	// Logging: Debug log for CurrentSelectedTag initialization
+	UE_LOG(LogTemp, Log, TEXT("CurrentSelectedTag Initialized To_selectionbox_constructor: %s"), *CurrentSelectedTag);
 
 	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -93,7 +100,11 @@ ASelectionBox::ASelectionBox()
 void ASelectionBox::BeginPlay()
 {
 	Super::BeginPlay();
+
 	UE_LOG(LogTemp, Log, TEXT("BeginPlay_selectionbox_beginplay"));
+
+	// Logging: Debug log for initial value check
+	UE_LOG(LogTemp, Log, TEXT("Initial Values - bIsControlPointSelected: %d, CurrentSelectedTag_selectionbox_beginplay: %s"), bIsControlPointSelected, *CurrentSelectedTag);
 }
 
 // Called every frame
@@ -101,35 +112,6 @@ void ASelectionBox::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Declare and initialize current mouse position variable
-	FVector2D currentMousePosition = FVector2D(0, 0);
-
-	// Declare control point variable as a static mesh component
-	UStaticMeshComponent* ControlPoint = nullptr;
-
-	// Check if a control point is selected for dragging
-	if (bIsControlPointSelected && ControlPoint)
-	{
-		// Get current mouse position
-		FVector2D CurrentMousePostion;
-		GetWorld()->GetFirstPlayerController()->GetMousePosition(currentMousePosition.X, currentMousePosition.Y);
-
-		// Convert mouse position to world coordinates
-		FVector worldLocation, worldDirection;
-		GetWorld()->GetFirstPlayerController()->DeprojectScreenPositionToWorld(currentMousePosition.X, currentMousePosition.Y, worldLocation, worldDirection);
-
-		// Update the control point's position to follow the mouse
-		FVector NewLocation = worldLocation + worldDirection;
-		ControlPoint->SetWorldLocation(NewLocation);
-
-		// Logging: Debug log for updating control point position
-		UE_LOG(LogTemp, Log, TEXT("Control Point Position Updated_selectionbox_tick"));
-	}
-	else
-	{
-		// Logging: Debug log for control point not selected or null
-		UE_LOG(LogTemp, Error, TEXT("Control Point Not Selected Or Is Null_selectionbox_tick"));
-	}
 }
 
 void ASelectionBox::DestroyMesh()
