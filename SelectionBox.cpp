272c4a7193205b7ba7e95cae1a03438247301ef0
BalmunAzuresky <40233917+AnthonyWinrow@users.noreply.github.com>
2023-09-05 21:57:49 -0700

    Update SelectionBox.cpp

--- SelectionBox.cpp
+++ SelectionBox.cpp
@@ -9,20 +9,14 @@ ASelectionBox::ASelectionBox()
 {
 	UE_LOG(LogTemp, Log, TEXT("SelectionBox Constructor Called_selectionbox_constructor"));
 
-	CurrentSelectedTag = TEXT("");
 	TimeSinceLeftMousePressed = 0.0f;
 	bIsLeftMouseButtonHeld = false;
 	bIsLeftMousePressed = false;
 	bIsDragging = false;
+	bHasCapturedInitialLocation = false;
 
 	// Logging: Variable Initializations
 	UE_LOG(LogTemp, Log, TEXT("SelectionBox Variables Initialized_selectionbox_constructor"));
-
-	// Logging: Debug log for current selected initialization
-	UE_LOG(LogTemp, Log, TEXT("CurrentSelectedTag Initialized To_selectionbox_constructor: %s"), *CurrentSelectedTag);
-
-	// Logging: Debug log for CurrentSelectedTag initialization
-	UE_LOG(LogTemp, Log, TEXT("CurrentSelectedTag Initialized To_selectionbox_constructor: %s"), *CurrentSelectedTag);
 
 	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -79,52 +73,49 @@ ASelectionBox::ASelectionBox()
 	FVector LeftPosition = Center - (WallRightVector * Offset);
 	FVector RightPosition = Center + (WallRightVector * Offset);
 
-	// Create the control point visualizations
-	TArray<FVector> ControlPointLocations = { LeftPosition, RightPosition };
-	for (int i = 0; i < ControlPointLocations.Num(); ++i)
-	{
-		FVector controlPointPosition = ControlPointLocations[i];
+	// Create the control point visualizations for ControlPointPointMesh0
+	ControlPointMesh0 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ControlPointMesh0"));
+	ControlPointMesh0->SetupAttachment(RootComponent);
+	ControlPointMesh0->SetStaticMesh(SphereMesh);
+	ControlPointMesh0->SetRelativeLocation(LeftPosition);
+	FVector ScaleFactor0 = FVector(0.1f, 0.1f, 0.1f);
+	ControlPointMesh0->SetWorldScale3D(ScaleFactor0);
 
-		// Logging: Verifying control point position
-		UE_LOG(LogTemp, Log, TEXT("ControlPoint%d Position_selectionbox_constructor: %s"), i, *ControlPointLocations[i].ToString());
+	ControlPointMesh1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ControlPointMesh1"));
+	ControlPointMesh1->SetupAttachment(RootComponent);
+	ControlPointMesh1->SetStaticMesh(SphereMesh);
+	ControlPointMesh1->SetRelativeLocation(RightPosition);
+	FVector ScaleFactor1 = FVector(0.1f, 0.1, 0.1f);
+	ControlPointMesh1->SetWorldScale3D(ScaleFactor1);
 
-		UStaticMeshComponent* ControlPointMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName(*FString::Printf(TEXT("ControlPointMesh%d"), i)));
-		ControlPointMesh->SetupAttachment(RootComponent);
-		ControlPointMesh->SetStaticMesh(SphereMesh);
-
-		// Logging: Verify static mesh set
-		UE_LOG(LogTemp, Log, TEXT("ControlPoint%d StaticMesh Set_selectionbox_constructor"), i);
-
-		// Set the relative location to the left or right position
-		ControlPointMesh->SetRelativeLocation(ControlPointLocations[i]);
-
-		// Logging: Verify relative location set
-		UE_LOG(LogTemp, Log, TEXT("ControlPoint%d Relative Location Set_selectionbox_constructor: %s"), i, *ControlPointLocations[i].ToString());
-
-		// Set the mesh scale factor
-		FVector ScaleFactor = FVector(0.1f, 0.1f, 0.1f);
-		ControlPointMesh->SetWorldScale3D(ScaleFactor);
-
-		// Logging: Verify mesh scale factor set
-		UE_LOG(LogTemp, Log, TEXT("ControlPoint%d Scale Factor Set_selectionbox_constructor: %s"), i, *ScaleFactor.ToString());
-
-		ControlPointMeshes.Add(ControlPointMesh);
-	}
-
+	// Logging: Verify control point initialization
 	UE_LOG(LogTemp, Log, TEXT("Control Points Initialized and Visible with Default Sphere Mesh_selectionbox_constructor"));
-	UE_LOG(LogTemp, Log, TEXT("Mesh Initialized in Constructor_selectionbox_constructor"));
 }
 
 // Called when the game starts or when spawned
 void ASelectionBox::BeginPlay()
 {
 	Super::BeginPlay();
+	UE_LOG(LogTemp, Log, TEXT("BeginPlay Now Called_selectionbox_beginplay"));
 
+	// Store the location of ControlPointMesh0
+	InitialLocationOfControlPointMesh0 = ControlPointMesh0->GetComponentLocation();
+
+	UE_LOG(LogTemp, Log, TEXT("InitialLocationOfControlPoint0_selectionbox_beginplay: %s"), *InitialLocationOfControlPointMesh0.ToString());
+
+	InitialLocationOfControlPointMesh1 = ControlPointMesh1->GetComponentLocation();
+
+	UE_LOG(LogTemp, Log, TEXT("InitialLocationOfControlPoint1_selectionbox_beginplay: %s"), *InitialLocationOfControlPointMesh1.ToString());
 }
 
 // Called every frame
 void ASelectionBox::Tick(float DeltaTime)
 {
+	UE_LOG(LogTemp, Log, TEXT("Tick Now Called_selectionbox_tick"));
+
+	// Logging Verify initial location of ControlPointMesh0
+	UE_LOG(LogTemp, Log, TEXT("InitialLocationOfControlPointMesh0_selectionbox_tick: %s"), *InitialLocationOfControlPointMesh0.ToString());
+
 	Super::Tick(DeltaTime);
 
 	// Initialize a variable to keep track of whether the cursor is over a control point mesh
@@ -158,20 +149,22 @@ void ASelectionBox::Tick(float DeltaTime)
 		// Check if raycast hit something
 		if (GetWorld()->LineTraceSingleByChannel(hitResult, start, end, ECC_Visibility, collisionParams))
 		{
-			// Loop through the spline mesh array to check if hit component was one of the spline meshes
-			for (int i = 0; i < ControlPointMeshes.Num(); ++i)
+			// Check if hit component was one of the control point meshes
+			if (hitResult.GetComponent()->IsA(UStaticMeshComponent::StaticClass()))
 			{
-				if (hitResult.GetComponent() == ControlPointMeshes[i])
+				UStaticMeshComponent* hitMesh = Cast<UStaticMeshComponent>(hitResult.GetComponent());
+
+				if (hitMesh == ControlPointMesh0 || hitMesh == ControlPointMesh1)
 				{
+					UE_LOG(LogTemp, Log, TEXT("Cursor is Over a Control Point"));
 					bIsCursorOverControlPoint = true;
 
 					// If mouse is clicked, set the selected control point mesh
 					if (bIsLeftMousePressed)
 					{
-						SelectedControlPoint = ControlPointMeshes[i];
+						UE_LOG(LogTemp, Log, TEXT("Control Point Selected_selectionbox_tick"));
+						SelectedControlPoint = hitMesh;
 					}
-
-					break;
 				}
 			}
 		}
@@ -186,6 +179,16 @@ void ASelectionBox::Tick(float DeltaTime)
 			PlayerController->CurrentMouseCursor = EMouseCursor::Default;
 		}
 
+		// Hide the mouse cursor when dragging
+		if (bIsDragging)
+		{
+			PlayerController->bShowMouseCursor = false;
+		}
+		else
+		{
+			PlayerController->bShowMouseCursor = true;
+		}
+
 		// Check if left mouse button is held for 0.5 seconds
 		if (TimeSinceLeftMousePressed > 0.5 && bIsLeftMousePressed)
 		{
@@ -195,7 +198,7 @@ void ASelectionBox::Tick(float DeltaTime)
 			if (GetWorld()->LineTraceSingleByChannel(hitResult, start, end, ECC_Visibility, collisionParams))
 			{
 				// Move the selected control point mesh with the mouse
-				if (SelectedControlPoint && hitResult.bBlockingHit)
+				if ((SelectedControlPoint == ControlPointMesh0) || (SelectedControlPoint == ControlPointMesh1) && hitResult.bBlockingHit)
 				{
 					// Initialize StartingLocation when control point is first selected
 					if (!bIsDragging)
@@ -220,27 +223,33 @@ void ASelectionBox::Tick(float DeltaTime)
 					FVector Direction = (ProjectedLocation - StartingLocation).GetSafeNormal();
 
 					// Smoothly Interpolate to the target world location
-					FVector SmoothWorldLocation = FMath::VInterpTo(CurrentWorldLocation, ProjectedLocation, DeltaTime, 5.0f);
+					FVector SmoothWorldLocation = FMath::VInterpTo(CurrentWorldLocation, ProjectedLocation, DeltaTime, 3.0f);
 
 					// Move the control point to the SmoothWorldLocation
 					SelectedControlPoint->SetWorldLocation(SmoothWorldLocation);
 
 					static float InitialDotProduct = FLT_MAX;
 
-					// Clamp ControlPointMesh0
-					if (SelectedControlPoint->GetName().Equals("ControlPointMesh0"))
+					// Clamp ControlPointmeshes
+					if (SelectedControlPoint == ControlPointMesh0)
 					{
+						// If this is the first time we're dragging this control point, store its initial location
+						if (!bIsDragging)
+						{
+							InitialLocationOfControlPointMesh0 = SelectedControlPoint->GetComponentLocation();
+						}
+
 						FVector WallRight = StaticMeshComponent->GetRightVector();
 						FVector CurrentLocation = SelectedControlPoint->GetComponentLocation();
 
 						// Logging: Verify WallRight and CurrentLocation
 						UE_LOG(LogTemp, Log, TEXT("WallRight: %s, CurrentLocation_selectionbox_tick: %s"), *WallRight.ToString(), *CurrentLocation.ToString());
 
-						// Calculate the distance from starting location
-						float Distance = FVector::Dist(StartingLocation, ProjectedLocation);
+						// Calculate the distance from the initial location
+						float Distance = FVector::Dist(InitialLocationOfControlPointMesh0, CurrentLocation);
 
 						// Determine the direction of movement
-						FVector MovementDirection = (ProjectedLocation - StartingLocation).GetSafeNormal();
+						FVector MovementDirection = (CurrentLocation - InitialLocationOfControlPointMesh0).GetSafeNormal();
 
 						// Logging: Verify WallRight, MovementDirection, and ProjectedLocation
 						UE_LOG(LogTemp, Log, TEXT("WallRight: %s, MovementDirection: %s, ProjectedLocation_selectionbox_tick: %s"), *WallRight.ToString(), *MovementDirection.ToString(), *ProjectedLocation.ToString());
@@ -248,20 +257,73 @@ void ASelectionBox::Tick(float DeltaTime)
 						// Calculate DotProduct
 						float DotProduct = FVector::DotProduct(MovementDirection, WallRight);
 
+						// Calculate the vector from the wall to the control point
+						FVector WallToControlPoint = CurrentLocation - InitialLocationOfControlPointMesh0;
+
+						// Calculate the dot product to check alighnment
+						float AlignmentDotProduct = FVector::DotProduct(WallToControlPoint.GetSafeNormal(), WallNormal);
+
 						// Logging: Verify dot product
 						UE_LOG(LogTemp, Log, TEXT("DotProduct_selectionbox_tick: %f"), DotProduct);
 
-						// Stabilize the direction based on the initial DotProduct
-						if ((InitialDotProduct < 0 && DotProduct < 0) || (InitialDotProduct >= 0 && DotProduct >= 0))
+						// If the control point is behind the wall, snap it back
+						if (AlignmentDotProduct < 0)
 						{
-							// Clamp the distance
-							float ClampedDistance = FMath::Clamp(Distance, 0.0f, 300.0f);
+							UE_LOG(LogTemp, Warning, TEXT("Control Point is Behind Wall, Resetting_selectionbox_tick"));
+							SelectedControlPoint->SetWorldLocation(InitialLocationOfControlPointMesh0);
+						}
+						// Only allow movement to the left of the wall
+						else if (DotProduct < 0)
+						{
+							// Clamp the distance so that it doesn't go past initial location
+							float ClampedDistance = FMath::Max(Distance, 0.0f);
 
 							// Calculate the new clamped location
-							FVector ClampedLocation = StartingLocation + WallRight * ClampedDistance;
+							FVector ClampedLocation = InitialLocationOfControlPointMesh0 + WallRight * -ClampedDistance;
 
-							// Logging: Verify the clamped location
-							UE_LOG(LogTemp, Log, TEXT("Clamped Location_selectionbox_tick: %s"), *ClampedLocation.ToString());
+							UE_LOG(LogTemp, Log, TEXT("Moving Control Point to Clamped Location_selectionbox_tick: %s"), *ClampedLocation.ToString());
+
+							// Set new clamped location
+							SelectedControlPoint->SetWorldLocation(ClampedLocation);
+						}
+						else
+						{
+							// Reset to initial location if trying to move to the right
+							SelectedControlPoint->SetWorldLocation(InitialLocationOfControlPointMesh0);
+						}
+					}
+					else if (SelectedControlPoint == ControlPointMesh1)
+					{
+						if (!bIsDragging)
+						{
+							InitialLocationOfControlPointMesh1 = SelectedControlPoint->GetComponentLocation();
+						}
+
+						FVector WallRight = StaticMeshComponent->GetRightVector();
+						FVector CurrentLocation = SelectedControlPoint->GetComponentLocation();
+
+						// Calculate the distance from initial location
+						float Distance = FVector::Dist(InitialLocationOfControlPointMesh1, CurrentLocation);
+
+						// Determine the direction of movement
+						FVector MovementDirection = (CurrentLocation - InitialLocationOfControlPointMesh1).GetSafeNormal();
+
+						// Calculate dot product
+						float DotProduct = FVector::DotProduct(MovementDirection, WallRight);
+
+						// If the control point is behind the wall, snap it back
+						if (DotProduct < 0)
+						{
+							SelectedControlPoint->SetWorldLocation(InitialLocationOfControlPointMesh1);
+						}
+						// Only allow movement to the right of the wall
+						else if (DotProduct > 0)
+						{
+							// Clamp the distance so that it doesn't go past initial position
+							float ClampedDistance = FMath::Max(Distance, 0.0f);
+
+							// Calculate new clamped location
+							FVector ClampedLocation = InitialLocationOfControlPointMesh1 + WallRight * ClampedDistance;
 
 							// Set new clamped location
 							SelectedControlPoint->SetWorldLocation(ClampedLocation);
@@ -287,6 +349,20 @@ void ASelectionBox::LeftClick(bool bIsPressed)
 
 	// Logging: State of left bIsLeftMousePressed
 	UE_LOG(LogTemp, Log, TEXT("LeftClick Called_selectionbox_leftclick: %s"), bIsLeftMousePressed ? TEXT("True") : TEXT("False"));
+
+	if (bIsLeftMousePressed && !bHasCapturedInitialLocation)
+	{
+		// Capture initial location only once
+		bHasCapturedInitialLocation = true;
+		InitialLocationOfControlPointMesh0 = ControlPointMesh0->GetComponentLocation();
+		UE_LOG(LogTemp, Log, TEXT("Captured Initial Location_selectionbox_leftclick: %s"), *InitialLocationOfControlPointMesh0.ToString());
+	}
+
+	if (!bIsLeftMousePressed)
+	{
+		// Reset dragging state when the button is released
+		bIsDragging = false;
+	}
 }
 
 void ASelectionBox::DestroyMesh()
