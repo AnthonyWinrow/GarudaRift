1378ec844ee2deb40284b18a58592b917407aec8
BalmunAzuresky <40233917+AnthonyWinrow@users.noreply.github.com>
2023-09-13 21:33:06 -0700

    Update SelectionBox.cpp

--- SelectionBox.cpp
+++ SelectionBox.cpp
@@ -20,50 +20,6 @@ ASelectionBox::ASelectionBox()
 	UE_LOG(LogSelectionBox_Constructor, Warning, TEXT("Entering Constructor"));
 
 	PrimaryActorTick.bCanEverTick = true;
-	bHasLoggedTickEntry = false;
-	TimeSinceLeftMousePressed = 0.0f;
-	bIsLeftMouseButtonHeld = false;
-	bIsLeftMousePressed = false;
-	bIsDragging = false;
-
-	WallSpline = CreateDefaultSubobject<USplineComponent>(TEXT("WallSpline"));
-	RootComponent = WallSpline;
-
-	SplinePointMetadata.Add(0, "ControlPoint0");
-	FString* MetaDataValue = SplinePointMetadata.Find(0);
-	if (MetaDataValue)
-	{
-		const TCHAR* MetaDataChar = **MetaDataValue;
-		UE_LOG(LogSelectionBox_Constructor, Log, TEXT("MetaData fo ControlPoint0 Added Successfully: %s"), MetaDataChar);
-	}
-	else
-	{
-		UE_LOG(LogSelectionBox_Constructor, Error, TEXT("Faled to Add MetaData for ControlPoint0"));
-	}
-
-	WallSplineMesh = CreateDefaultSubobject<USplineMeshComponent>(TEXT("WallSplineMesh"));
-	WallSplineMesh->SetMobility(EComponentMobility::Movable);
-	WallSplineMesh->SetupAttachment(RootComponent);
-
-	ControlPointMesh0 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ControlPointMesh0"));
-	ControlPointMesh0->SetupAttachment(WallSpline);
-
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMeshAsset(TEXT("StaticMesh'/Engine/BasicShapes/Sphere.Sphere'"));
-	if (SphereMeshAsset.Succeeded())
-	{
-		ControlPointMesh0->SetStaticMesh(SphereMeshAsset.Object);
-		UE_LOG(LogSelectionBox_BeginPlay, Log, TEXT("ControlPointMesh0 Initialized and Mesh Attached"));
-	}
-
-	FVector LocalForwardVector = WallSplineMesh->GetForwardVector();
-	FVector LocalLeftOffset = -LocalForwardVector * 5.0f;
-
-	ControlPointMesh0->SetRelativeLocation(LocalLeftOffset);
-	UE_LOG(LogSelectionBox, Log, TEXT("ControlPointMesh0 Local Left Offset Set to: %s"), *LocalLeftOffset.ToString());
-
-	FVector Scale(0.13f, 0.13f, 0.13f);
-	ControlPointMesh0->SetWorldScale3D(Scale);
-	UE_LOG(LogSelectionBox_Constructor, Log, TEXT("ControlPointMesh0 Scale to: %s"), *Scale.ToString());
 }
 
 //Called when the game starts or when spawned
@@ -73,33 +29,6 @@ void ASelectionBox::BeginPlay()
 	
 	Super::BeginPlay();
 
-	Wall = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), NULL, TEXT("StaticMesh'/Game/GarudaRift/Meshes/Architecture/VFX/SelectionBox_garudarift.SelectionBox_garudarift'")));
-	if (Wall)
-	{
-		WallSplineMesh->SetStaticMesh(Wall);
-		UE_LOG(LogSelectionBox_BeginPlay, Log, TEXT("Successfully Set the Static Mesh for WallSplineMesh"));
-	}
-	else
-	{
-		UE_LOG(LogSelectionBox_BeginPlay, Error, TEXT("Failed to Set the Static Mesh for WallSplineMesh"));
-	}
-	FVector WallScale = FVector(1.0f, 0.4f, 1.0f);
-	WallSplineMesh->SetRelativeScale3D(WallScale);
-	UE_LOG(LogSelectionBox_BeginPlay, Log, TEXT("WallSplineMesh Scale Set to: %s"), *WallScale.ToString());
-
-	FVector ZOffset = FVector(0, 0, 150.0f);
-	ControlPointMesh0->AddRelativeLocation(ZOffset);;
-	UE_LOG(LogSelectionBox_BeginPlay, Log, TEXT("ControlPointMesh0 Z-Offset Set to: %s"), *ZOffset.ToString());
-
-	if (ControlPointMesh0)
-	{
-		InitialLocationOfControlPointMesh0 = ControlPointMesh0->GetComponentLocation();
-		UE_LOG(LogSelectionBox_BeginPlay, Log, TEXT("Initial Location of ControlPointMesh0 Stored: %s"), *InitialLocationOfControlPointMesh0.ToString());
-	}
-	else
-	{
-		UE_LOG(LogSelectionBox, Error, TEXT("ControlPointMesh0 Is Not Initialized"));
-	}
 }
 
 // Called every frame
@@ -109,7 +38,7 @@ void ASelectionBox::Tick(float DeltaTime)
 
 	if (bIsLeftMousePressed)
 	{
-		UE_LOG(LogSelectionBox_Tick, Log, TEXT("bIsLefMousePressed = True"));
+		UE_LOG(LogSelectionBox_Tick, Log, TEXT("bIsLeftMousePressed = True"));
 		TimeSinceLeftMousePressed += DeltaTime;
 		if (TimeSinceLeftMousePressed >= 0.2f && !bIsLeftMouseButtonHeld)
 		{
@@ -149,14 +78,55 @@ void ASelectionBox::Tick(float DeltaTime)
 			if (bIsDragging)
 			{
 				PlayerController->bShowMouseCursor = false;
-				FVector NewLocation = HitResult.Location;
 
+				FVector WallForward = WallSplineMesh->GetForwardVector();
+				TargetLocation = FVector::DotProduct(HitResult.Location - WallSplineMesh->GetComponentLocation(), WallForward) * WallForward + WallSplineMesh->GetComponentLocation();
+				
+				FVector CurrentLocation = ControlPointMesh0->GetComponentLocation();				
+				FVector NewLocation = FMath::Lerp(CurrentLocation, TargetLocation, 0.03f);
+
+				FVector CurrentControlPoint0Location = WallSpline->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::World);
+				
+				NewLocation.Z = CurrentLocation.Z;
+
+				float InitialProjection = FVector::DotProduct(InitialLocationOfControlPointMesh0, WallForward);
+				float NewProjection = FVector::DotProduct(NewLocation, WallForward);
+				float Buffer = 0.1f;
+
+				if (NewProjection > InitialProjection + Buffer)
+				{
+					NewLocation = InitialLocationOfControlPointMesh0;
+
+					FVector2D ScreenMousePosition;
+					PlayerController->GetMousePosition(ScreenMousePosition.X, ScreenMousePosition.Y);
+					FVector2D ScreenLocation;
+					PlayerController->ProjectWorldLocationToScreen(NewLocation, ScreenLocation);
+					PlayerController->SetMouseLocation(ScreenLocation.X, ScreenLocation.Y);
+				}
+
+				ControlPointMesh0->SetWorldLocation(NewLocation);
+
+				ControlPoint0Location = ControlPointMesh0->GetComponentLocation();
+				ControlPoint1Location = ControlPointMesh1->GetComponentLocation();
+
+				NewLocation.Z = CurrentControlPoint0Location.Z;
+
+				WallSpline->SetLocationAtSplinePoint(0, ControlPoint0Location, ESplineCoordinateSpace::World);
+				WallSpline->SetLocationAtSplinePoint(1, ControlPoint1Location, ESplineCoordinateSpace::World);
+				WallSpline->UpdateSpline();
+
+				FVector SplinePoint0Location = WallSpline->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::World);
+				FVector SplinePoint1Location = WallSpline->GetLocationAtSplinePoint(1, ESplineCoordinateSpace::World);
+
+				FVector AutoStartTangent = WallSpline->GetTangentAtSplinePoint(0, ESplineCoordinateSpace::World).GetSafeNormal();
+				FVector AutoEndTangent = WallSpline->GetTangentAtSplinePoint(1, ESplineCoordinateSpace::World).GetSafeNormal();
+
+				WallSplineMesh->SetStartAndEnd(SplinePoint0Location, AutoStartTangent, SplinePoint1Location, AutoEndTangent);
 			}
 			else
 			{
 				PlayerController->bShowMouseCursor = true;
 			}
-
 		}
 	}
 }
