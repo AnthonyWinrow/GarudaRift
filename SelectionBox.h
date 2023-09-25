d279463ab7f26b7108b1a580e88fb5324943cc94
BalmunAzuresky <40233917+AnthonyWinrow@users.noreply.github.com>
2023-09-25 13:42:57 -0700

    Update SelectionBox.h

--- SelectionBox.h
+++ SelectionBox.h
@@ -1,84 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Components/StaticMeshComponent.h"
 #include "Components/SplineComponent.h"
-#include "Components/SplineMeshComponent.h"
 #include "SelectionBox.generated.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(LogSelectionBox, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSelectionBox_Constructor, Log, All);
-DECLARE_LOG_CATEGORY_EXTERN(LogSelectionBox_LeftClick, Log, All);
-DECLARE_LOG_CATEGORY_EXTERN(LogSelectionBox_DestroyMesh, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSelectionBox_BeginPlay, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogSelectionBox_Tick, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogSelectionBox_LeftClick, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogSelectionBox_DestroyMesh, Log, All);
 
 UCLASS()
 class GARUDARIFT_API ASelectionBox : public AActor
 {
 	GENERATED_BODY()
-
+	
 public:
-	// Sets default values for this actor's properties
+	// Sets default values for this actor's	 properties
 	ASelectionBox();
-
-	UPROPERTY()
-	FVector TargetLocation;
-
-	void DestroyMesh();
-	void LeftClick(bool bIsPressed);
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(VisibleAnywhere)
-	UStaticMeshComponent* StaticMeshComponent;
-
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* ControlPointMesh;
-
-	UPROPERTY(BlueprintReadWrite, Category = "Control Point")
-	bool bIsControlPointSelected;
-
-private:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spline", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	USplineComponent* WallSpline;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SplineMesh", meta = (AllowPrivateAccess = "true"))
-	USplineMeshComponent* WallSplineMesh;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Control Point", meta = (AllowPrivateAccess = "true"))
-	UStaticMeshComponent* ControlPointMesh0;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Control Point", meta = (AllowPrivateAccess = "true"))
-	UStaticMeshComponent* ControlPointMesh1;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Control Point", meta = (AllowPrivateAccess = "true"))
-	UStaticMeshComponent* SelectedControlPoint;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh")
+	UStaticMeshComponent* StaticMeshParent;
 
 	UPROPERTY()
-	TMap<int32, FString> SplinePointMetadata;
+	UStaticMeshComponent* SplinePointMesh0;
 
-	bool bIsDragging;
-	float TimeSinceLeftMousePressed;
-	bool bIsLeftMouseButtonHeld;
+	UPROPERTY()
+	UStaticMeshComponent* SplinePointMesh1;
+
+	void LeftClick(bool bIsPressed);
+	void DestroyMesh();
+
 	bool bIsLeftMousePressed;
-	bool bIsMouseOverControlPointMesh;
-	bool bHasLoggedTickEntry;
-	FVector StartingLocation;
-	FVector InitialLocationOfControlPointMesh0;
-	FVector InitialLocationOfControlPointMesh1;
-	FVector LeftPosition;
-	FVector RightPosition;
-	FVector Center;
-	FVector2D StoredInitialClickLocation;
-	UStaticMesh* Wall;
+	bool bIsDragging;
+	bool bMouseHeld;
+	bool bLastMouseHeld;
+	bool bLastIsDragging;
+	float TimeMousePressed;
 
-	bool bHasCapturedInitialLocation;
-	FVector InitialWallMeshLocation;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
